87d21c2453739c5d49b90934d382422d79296954
karlaathamiris <karlatn@outlook.com>
2024-10-09 12:08:26 -0300

    jogo finalizado

--- app.js
+++ app.js
@@ -1,19 +1,71 @@
-alert("Esse é o Jogo do Número Secreto, bem vinde!!!");
-let numeroSecreto = 29;
-let chute;
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10; 
+let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-while (chute != numeroSecreto) {
-    chute = prompt("Escolha um número entre 1 e 30: ")
-    
-    if (chute == numeroSecreto) {
-        alert (`ACERTOU! Realmente é ${numeroSecreto}. Você precisou de ${tentativas} tentativas`);
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2})
+}
+
+function exibirMensagemInicial() {
+    exibirTexto('h1', 'Jogo do número secreto. Ajude o Zé a achar o número secreto');
+    exibirTexto('p', 'Escolha um número entre 1 e 10')
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if(chute == numeroSecreto) {
+        exibirTexto('h1', 'Acertou');
+        //Foi preciso criar uma variável para mensagemTentativas pq o html espera
+        //como parâmetro em "exibirTexto" apenas Strings e a variável ${tentativas}
+        //é uma template String, que o JS entende
+        let tentativaSingular = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = (`Você descobriu o número secreto com ${tentativas} ${tentativaSingular}`);
+        exibirTexto('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        
     } else {
-        if(chute < numeroSecreto){
-            alert(`O número secreto é maior que ${chute}`);
-        }else{
-            alert(`O número secreto é menor que ${chute}`);
+        if (chute > numeroSecreto) {
+            exibirTexto('p', 'O número secreto é menor');
+        } else {
+            exibirTexto('p', 'O número secreto é maior')
         }
+        tentativas++
+        limparCampo();
     }
-    tentativas++;
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite +1);
+    let qtdElementosLista = listaDeNumerosSorteados.length;
+
+    if(qtdElementosLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio(); //recursssão
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
